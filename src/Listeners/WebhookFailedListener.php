902dd047dd3a33ab1670afa7b5b8546f14f382d9
andre-silvakmm <andre.silva@kmm.com.br>
2024-05-07 16:14:12 -0300

    Fix - fail response

--- src/Listeners/WebhookFailedListener.php
+++ src/Listeners/WebhookFailedListener.php
@@ -23,7 +23,7 @@ class WebhookFailedListener
                 $webhookClientHistory->webhook_client = $event->meta['webhookClient'];
                 $webhookClientHistory->uuid = $event->uuid;
                 $webhookClientHistory->status_code = $event->response !== null ? $event->response->getStatusCode() : null;
-                $webhookClientHistory->errorMessage = $event->response !== null ? $event->response->getReasonPhrase() : $body->getContents();
+                $webhookClientHistory->errorMessage = $body->getContents() !== null && $body->getContents() !== '' ? $body->getContents() : $event->response->getReasonPhrase();
                 $webhookClientHistory->errorType = $event->errorType;
                 $webhookClientHistory->attempt = $event->attempt;
 
