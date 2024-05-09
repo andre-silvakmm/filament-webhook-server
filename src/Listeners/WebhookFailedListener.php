7d92648c143ce812cc0f5fc4f68eee6c9dc830d6
andre-silvakmm <andre.silva@kmm.com.br>
2024-05-09 16:08:00 -0300

    Fix - error message failed listener

--- src/Listeners/WebhookFailedListener.php
+++ src/Listeners/WebhookFailedListener.php
@@ -16,14 +16,12 @@ class WebhookFailedListener
     public function handle($event)
     {
         if (config('filament-webhook-server.webhook.keep_history')) {
-            $body = $event->response->getBody();
-
             try {
                 $webhookClientHistory = new FilamentWebhookServerHistory();
                 $webhookClientHistory->webhook_client = $event->meta['webhookClient'];
                 $webhookClientHistory->uuid = $event->uuid;
                 $webhookClientHistory->status_code = $event->response !== null ? $event->response->getStatusCode() : null;
-                $webhookClientHistory->errorMessage = $body->getContents() !== null && $body->getContents() !== '' ? $body->getContents() : $event->response->getReasonPhrase();
+                $webhookClientHistory->errorMessage = $event->errorMessage;
                 $webhookClientHistory->errorType = $event->errorType;
                 $webhookClientHistory->attempt = $event->attempt;
 
